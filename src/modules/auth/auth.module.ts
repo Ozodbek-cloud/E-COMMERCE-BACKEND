546f235b3de6b917b366c185b932556135b19a88
Ozodbek-cloud <vortextblade16@gmail.com>
2025-09-10 15:08:51 +0500

    .

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtAccessTokenModule } from 'src/common/jwt/jwt.access.module';
-import { JwtRefreshTokenModule } from 'src/common/jwt/jwt.refresh.module';
-import { MailModule } from 'src/common/mail/mail.module'; 
-import { RedisModule } from 'src/common/redis/redis.module';
+import { JwtAccessTokenModule } from '../../common/jwt/jwt.access.module';
+import { JwtRefreshTokenModule } from '../../common/jwt/jwt.refresh.module';
+import { MailModule } from '../../common/mail/mail.module'; 
+import { RedisModule } from '../../common/redis/redis.module';
 import { JwtModule } from '@nestjs/jwt';
-import { PrismaService } from 'src/core/prisma/prisma.service';
+import { PrismaService } from '../../core/prisma/prisma.service';
 @Module({
   imports: [JwtAccessTokenModule, JwtRefreshTokenModule, MailModule, RedisModule, JwtModule],
   controllers: [AuthController],
